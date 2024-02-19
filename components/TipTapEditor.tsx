aeac042e3d6e5f5ee999f02d6f9ba449304f6af5
Joe Richens <jrichens@gmail.com>
2024-02-19 15:36:16 +0000

    ESLint upgrades 3

--- components/TipTapEditor.tsx
+++ components/TipTapEditor.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { GetTaskDetails } from "@/actions/get-task-details"
 import { EditorContent, useEditor } from "@tiptap/react"
 import { StarterKit } from "@tiptap/starter-kit"
 import { useEffect, useState } from "react"
@@ -33,20 +32,18 @@ const TipTapEditor = ({ initialContent }: Props) => {
 
   return (
     <div className="h-[600px] max-w-screen-md">
-      {(editor?.isFocused || editorClicked) && (
-        <>
-          <div className="flex flex-row space-x-2 items-center justify-center pb-2 mb-1 border-b-2 border-b-gray-300">
-            <TipTapMenu editor={editor} />
-            <Button
-              onClick={() => {
-                // Save our new daily check into database
-                setSaveClicked(true)
-              }}
-            >
-              Save
-            </Button>
-          </div>
-        </>
+      {editor && (editor.isFocused || editorClicked) && (
+        <div className="flex flex-row space-x-2 items-center justify-center pb-2 mb-1 border-b-2 border-b-gray-300">
+          <TipTapMenu editor={editor} />
+          <Button
+            onClick={() => {
+              // Save our new daily check into database
+              setSaveClicked(true)
+            }}
+          >
+            Save
+          </Button>
+        </div>
       )}
 
       <article className="prose lg:prose-xl">
