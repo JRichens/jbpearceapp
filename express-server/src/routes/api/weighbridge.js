b633704d5deb96f6655f28a9bf8f14e3ed266b37
Joe Richens <jrichens@gmail.com>
2024-02-20 09:48:44 +0000

    Stop tracking certain files

--- express-server/src/routes/api/weighbridge.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const weighbridgeController = require("../../controllers/weighbridge")
-
-router.route("/").get(weighbridgeController.getWeight)
-
-module.exports = router
