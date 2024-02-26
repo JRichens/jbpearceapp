41a5447351d735cd02f8cc943c709838e7e2db14
Joe Richens <jrichens@gmail.com>
2024-02-26 15:03:27 +0000

    new_favIcons2

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css"
 import { siteConfig } from "@/config/site"
 import { ClerkProvider } from "@clerk/nextjs"
 import { Toaster } from "@/components/ui/toaster"
-import Head from "next/head"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -29,42 +28,6 @@ export default function RootLayout({
         },
       }}
     >
-      <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link
-          rel="manifest"
-          href="/site.webmanifest"
-        />
-        <link
-          rel="mask-icon"
-          href="/safari-pinned-tab.svg"
-          color="#5bbad5"
-        />
-        <meta
-          name="msapplication-TileColor"
-          content="#da532c"
-        />
-        <meta
-          name="theme-color"
-          content="#ffffff"
-        />
-      </Head>
       <html lang="en">
         <body className={inter.className}>
           {children}
