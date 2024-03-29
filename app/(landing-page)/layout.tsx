697fcbc5dcbca7197d1e4e0f973a9695a4cdbea1
Joe Richens <jrichens@gmail.com>
2024-03-29 11:24:06 +0000

    majorLandAreaUpdateFix02

--- app/(landing-page)/layout.tsx
+++ app/(landing-page)/layout.tsx
@@ -6,7 +6,10 @@ const LandingLayout = ({ children }: { children: React.ReactNode }) => {
     <>
       <Navbar />
       <div className="flex">
-        <Sidebar />
+        <div className="w-60 shrink-0 hidden md:block shadow-[10px_0_5px_0px_rgba(0,0,0,0.3)]">
+          <Sidebar />
+        </div>
+
         <div className="pt-20 min-h-screen grainy flex-grow">{children}</div>
       </div>
     </>
