d692133c1a0212989930f7420e4f33c2a188d9a4
JRichens <jrichens@gmail.com>
2025-01-13 16:16:29 +0000

    UploadThing Fix 002

--- actions/del-exportPhotos.ts
+++ actions/del-exportPhotos.ts
@@ -3,16 +3,32 @@
 import { UTApi } from 'uploadthing/server'
 import { auth } from '@clerk/nextjs'
 import { revalidatePath } from 'next/cache'
+import { db } from '@/lib/db'
 
 const utapi = new UTApi()
 
-export async function DeletePhoto(photo: string) {
+export async function DeleteExportPhoto(
+    photo: string,
+    carReg: string,
+    newPhotos: string[]
+) {
     const { userId }: { userId: string | null } = auth()
 
     if (userId) {
         try {
             await utapi.deleteFiles(photo)
-            console.log(`${userId} Deleted Photo: ${photo}`)
+            console.log(`${userId} Deleted Photo: ${photo} from ${carReg}`)
+
+            // Update the database with the new photos array
+            await db.exporting.update({
+                where: {
+                    carReg: carReg,
+                },
+                data: {
+                    photos: newPhotos,
+                },
+            })
+
             revalidatePath('/vehicles-export')
         } catch (error) {
             console.log(error)
